6fa19466e40dabdeaa0c0b513898e453adc0634f
Avi Rai <82499697+Cvr421@users.noreply.github.com>
2022-06-21 16:11:06 +0530

    Create s3.js
    Here we are Deploying our app into AWS server on s3

---
+++ src/config/s3.js
@@ -0,0 +1,40 @@
+
+// install aws-sdk we have to install the npm package for aws
+const AWS = require('aws-sdk');
+const fs = require('fs');
+require('dotenv').config();
+
+// console.log(process.env);
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+AWS.config.update({
+    accessKeyId,
+    secretAccessKey
+});
+
+var s3 = new AWS.S3();
+// taking the mmage file from our file and server
+const uploadFile = function(file) {
+    console.log("File object\n", file);
+    const fileStream = fs.createReadStream(file.path);
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    };
+    console.log("Hi");
+    return s3.upload(uploadParams).promise();//if weuse the promise then it will give the promise object
+}
+
+const getFile = function(key) {
+    const params = {
+        Key: key,
+        Bucket: bucketName
+    };
+    return s3.getObject(params).createReadStream();
+}
+
+module.exports = {uploadFile, getFile};
